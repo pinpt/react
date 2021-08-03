5dff4eecc8fa55ccebb970f79bed8ae2ed0d0abf
Keegan Donley <kdonley@pinpoint.com>
2021-08-03 15:19:43 -0500

    switch to tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,24 @@
+const path = require('path')
+
 module.exports = {
 	stories: ['../**/*.stories.mdx', '../**/*.stories.@(js|jsx|ts|tsx)'],
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-jest'],
+	webpackFinal: async (config) => {
+		config.module.rules.push({
+			test: /\.css$/,
+			use: [
+				{
+					loader: "postcss-loader",
+					options: {
+						// HERE: OPTIONS
+						postcssOptions: {
+							plugins: [require("tailwindcss"), require("autoprefixer")],
+						},
+					},
+				},
+			],
+			include: path.resolve(__dirname, "../"),
+		})
+		return config
+	}
 };
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./base.css'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
