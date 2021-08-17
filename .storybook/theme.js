94ec8ba5b5ae2d65d4e84be464c92e2a2cb206b5
Keegan Donley <kdonley@pinpoint.com>
2021-08-17 14:11:57 -0500

    add custom logo for storybook

---
+++ .storybook/theme.js
@@ -0,0 +1,10 @@
+import { create } from '@storybook/theming';
+
+import logo from './logo.svg';
+
+export default create({
+	base: 'light',
+	brandTitle: 'Pinpoint React',
+	brandUrl: 'https://pinpoint.com',
+	brandImage: logo,
+});
