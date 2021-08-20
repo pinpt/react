476a5a6696737cf73fbb970de155c5c01bccbc72
Keegan Donley <kdonley@pinpoint.com>
2021-08-20 14:41:03 -0500

    use new beacon to track entry views

--- src/components/Pinpoint/index.ts
+++ src/components/Pinpoint/index.ts
@@ -1,8 +1,9 @@
-import { ReactElement } from 'react';
+import { ReactElement, useEffect } from 'react';
 import useScriptLoader from '../../lib/hooks/useScriptLoader';
 
 export interface IPinpointProps {
 	siteId: string;
+	contentId?: string;
 	children: (ready: boolean, ref: any) => ReactElement;
 }
 
@@ -11,13 +12,22 @@ declare global {
 		iframely?: {
 			load: (elem?: HTMLElement, url?: string) => void;
 		};
+		Pinpoint: {
+			setSiteSettings: (siteId: string, contentId: string) => void;
+			startTracking: (siteId?: string, contentId?: string) => () => void;
+		};
 	}
 }
 
 const Pinpoint = (props: IPinpointProps) => {
-	const [ready] = useScriptLoader([
-		`https://cdn.iframe.ly/embed.js?api_key=ab49ad398c6f631ab44eca&origin=${props.siteId}`,
-	]);
+	const { siteId, contentId } = props;
+	const [ready] = useScriptLoader([`https://cdn.iframe.ly/embed.js?api_key=ab49ad398c6f631ab44eca&origin=${siteId}`]);
+
+	useEffect(() => {
+		const clear = window.Pinpoint?.startTracking(siteId, contentId);
+
+		return () => clear?.();
+	}, [siteId, contentId]);
 
 	const ref = () => {
 		if (ready && typeof window !== 'undefined') {
