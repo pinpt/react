455c375fbb8af215cc72e888e467f59d1e62000d
Keegan Donley <kdonley@pinpoint.com>
2021-09-09 13:27:19 -0500

    update types

--- src/lib/types/content.ts
+++ src/lib/types/content.ts
@@ -5,11 +5,42 @@ export enum CoverMediaType {
 	Youtube = 'youtube',
 }
 
+export interface CoverMediaProperties {
+	title?: {
+		text: string;
+		size: number;
+		foreground: string;
+		background: string;
+		radius: number;
+		font?: {
+			family: string;
+			weight?: number;
+		};
+	};
+	background?: {
+		src?: string;
+		inset: number;
+		radius: number;
+		color?: string;
+		gradient?: string;
+		fit: 'contain' | 'cover' | 'fill' | 'none' | 'scale-down';
+	};
+	metadata?: {
+		mimetype: string;
+		width?: number;
+		height?: number;
+		size?: number;
+		duration?: number;
+	};
+}
+
 export interface ICoverMedia {
 	type: CoverMediaType;
-	placeholderImage: string;
+	placeholderImage?: string;
 	value: string;
 	metadata?: Record<string, any>;
+	publicUrl?: string;
+	properties?: CoverMediaProperties;
 }
 
 export enum ContentTemplateType {
