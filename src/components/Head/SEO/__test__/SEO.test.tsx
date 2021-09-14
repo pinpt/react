93355ec9aaf2baf26babdf2a3cd12a2a5fce2913
Jeff Haynie <jhaynie@pinpoint.com>
2021-09-13 22:44:38 -0500

    Add support for private visibility which disables search index index (#117)
    * Add support for private visibility which disables search index index
    
    * 1.0.11

--- src/components/Head/SEO/__test__/SEO.test.tsx
+++ src/components/Head/SEO/__test__/SEO.test.tsx
@@ -10,3 +10,9 @@ test('Test default render', () => {
 	const tree = component.toJSON();
 	expect(tree).toMatchSnapshot();
 });
+
+test('Test private render', () => {
+	const component = renderer.create(<SEO site={{ ...site, private: true }} content={entries[0] as IContent} />);
+	const tree = component.toJSON();
+	expect(tree).toMatchSnapshot();
+});
