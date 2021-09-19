7d05faf5904c6e97b04d9b672cf371c5a2c05e60
Jeff Haynie <jhaynie@pinpoint.com>
2021-09-19 12:50:15 -0500

    theme switcher save (#126)

--- src/components/ThemeToggle/index.tsx
+++ src/components/ThemeToggle/index.tsx
@@ -3,10 +3,11 @@ import React, { useCallback } from 'react';
 export interface IThemeToggleProps {
 	className?: string;
 	onThemeChange?: (theme: 'light' | 'dark') => void;
+	localStorageKey?: string; // if you want to change the localstorage key. defaults to 'theme'
 }
 
 const ThemeToggle = (props: IThemeToggleProps) => {
-	const { className = '', onThemeChange } = props;
+	const { className = '', onThemeChange, localStorageKey = 'theme' } = props;
 
 	const handleToggleTheme = useCallback(() => {
 		const element = document.getElementsByTagName('html')?.[0];
@@ -14,12 +15,37 @@ const ThemeToggle = (props: IThemeToggleProps) => {
 			if (element.classList.contains('dark')) {
 				element.classList.remove('dark');
 				onThemeChange?.('light');
+				window.localStorage.setItem(localStorageKey, 'light');
 			} else {
 				element.classList.add('dark');
 				onThemeChange?.('dark');
+				window.localStorage.setItem(localStorageKey, 'dark');
 			}
 		}
-	}, [onThemeChange]);
+	}, [onThemeChange, localStorageKey]);
+
+	React.useEffect(() => {
+		if (typeof window !== 'undefined') {
+			const localTheme = window.localStorage.getItem(localStorageKey);
+			if (localTheme) {
+				const element = document.getElementsByTagName('html')?.[0];
+				switch (localTheme) {
+					case 'dark': {
+						element.classList.add('dark');
+						onThemeChange?.('dark');
+						break;
+					}
+					case 'light': {
+						element.classList.remove('dark');
+						onThemeChange?.('light');
+						break;
+					}
+					default:
+						break;
+				}
+			}
+		}
+	}, [localStorageKey, onThemeChange]);
 
 	return (
 		<div className={`Pinpoint ThemeToggle ${className}`}>
