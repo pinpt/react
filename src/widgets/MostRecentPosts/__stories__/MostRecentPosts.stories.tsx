37b76176f74ec19d9c23943751771933a62cce54
Jeff Haynie <jhaynie@pinpoint.com>
2021-10-27 13:15:51 -0500

    Add ability to have a different style and customized css for most recent posts widget

--- src/widgets/MostRecentPosts/__stories__/MostRecentPosts.stories.tsx
+++ src/widgets/MostRecentPosts/__stories__/MostRecentPosts.stories.tsx
@@ -22,3 +22,27 @@ export default {
 export const Default: React.VFC<{}> = () => (
 	<MostRecentPosts count={previewData.content.length} previewData={previewData as any} />
 );
+
+export const Style1: React.VFC<{}> = () => (
+	<MostRecentPosts count={previewData.content.length} previewData={previewData as any} style="style1" />
+);
+
+export const Style2: React.VFC<{}> = () => (
+	<MostRecentPosts count={previewData.content.length} previewData={previewData as any} style="style2" />
+);
+
+const inlineCSS = `
+.Pinpoint.Widget.MostRecentPosts.MyStyles .Tile.style2 {
+	color: #990000 !important;
+	font-family: "futura-pt-bold","Century Gothic",CenturyGothic,"Apple Gothic",AppleGothic,"URW Gothic L","Avant Garde",sans-serif !important;
+}
+`;
+export const Style2_With_Custom_CSS: React.VFC<{}> = () => (
+	<MostRecentPosts
+		count={previewData.content.length}
+		previewData={previewData as any}
+		style="style2"
+		inlineCSS={inlineCSS}
+		className="MyStyles"
+	/>
+);
--- src/widgets/MostRecentPosts/index.tsx
+++ src/widgets/MostRecentPosts/index.tsx
@@ -4,6 +4,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from '../../components/Image';
 import type { IContent } from '../../lib/types';
 
+type Styles = 'style1' | 'style2';
+
 interface PageProps {
 	count: number;
 	previewData: {
@@ -11,11 +13,14 @@ interface PageProps {
 		content: IContent[];
 	};
 	className?: string;
+	style?: Styles;
+	inlineCSS?: string;
+	cssID?: string;
 }
 
-const Tile = ({ content }: { content: IContent }) => {
+const Style1 = ({ content }: { content: IContent }) => {
 	return (
-		<div className="Tile">
+		<div className="style1">
 			<div className="covermedia">
 				<a href={content.url}>
 					{content.coverMedia?.placeholderImage ? (
@@ -36,12 +41,52 @@ const Tile = ({ content }: { content: IContent }) => {
 	);
 };
 
-const Page = (props: PageProps) => {
-	const { previewData, className = '' } = props;
+const Style2 = ({ content }: { content: IContent }) => {
 	return (
-		<div className={`Pinpoint Widget MostRecentPosts ${className}`}>
+		<div className="style2">
+			<div className="covermedia">
+				<a href={content.url}>
+					{content.coverMedia?.placeholderImage ? (
+						<Image src={content.coverMedia.placeholderImage} alt={content.title} className="image" />
+					) : (
+						<div className="empty">&nbsp;</div>
+					)}
+				</a>
+			</div>
+			<div className="body">
+				<a href={content.url}>
+					<div className="title">{content.title}</div>
+					<div className="link">
+						<span>Read More</span>
+					</div>
+				</a>
+			</div>
+		</div>
+	);
+};
+
+const Tile = ({ content, style }: { content: IContent; style: Styles }) => {
+	let comp: React.ReactNode;
+	switch (style) {
+		case 'style1':
+			comp = <Style1 content={content} />;
+			break;
+		case 'style2':
+			comp = <Style2 content={content} />;
+			break;
+		default:
+			break;
+	}
+	return <div className={`Tile ${style}`}>{comp}</div>;
+};
+
+const Page = (props: PageProps) => {
+	const { previewData, className = '', style = 'style1', inlineCSS } = props;
+	return (
+		<div className={`Pinpoint Widget MostRecentPosts ${className} ${style}`.trim()}>
+			{inlineCSS && <style dangerouslySetInnerHTML={{ __html: inlineCSS }} />}
 			{previewData.content.map((content) => (
-				<Tile key={content.id ?? content.url} content={content} />
+				<Tile key={content.id ?? content.url} content={content} style={style} />
 			))}
 		</div>
 	);
