ea2b0f56c128f5ccdb84ce34edf98cebc18b4b69
Keegan Donley <kdonley@pinpoint.com>
2021-11-12 15:44:47 -0600

    add missing react imports

--- src/components/RoadmapCard/index.tsx
+++ src/components/RoadmapCard/index.tsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export interface IRoadmapCardProps {
 	className?: string;
 	title: string;
--- src/components/RoadmapSection/index.tsx
+++ src/components/RoadmapSection/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { faChevronDown, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
